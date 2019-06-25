0b3cb9711f87d6046bb50fd975d18cefd8a76f41
Sven Berglund <sven.berglund@innovatesecurity.se>
2019-06-25 14:08:28 -0400

    Now a working minimalistic demo

--- minimal-demo/src/App.js
+++ minimal-demo/src/App.js
@@ -3,53 +3,63 @@ import './App.css';
 import RMGL from 'react-mst-grid-layout';
 import GridElementDeadSimple from './elements/GridElementDeadSimple'
 
-
 // Register our grid element implementation with a name
 RMGL.GridElementSuper.register("simple", GridElementDeadSimple);
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // Publishing mock data to the grid
+  handleSubmit(event){
+    RMGL.PubSubAPI.publish(1, {some : "awesome", other : "we can" });
+    setTimeout(function() {RMGL.PubSubAPI.publish(0, {some : "publish", other : "more" })}, 1000);
+    setTimeout(function() {RMGL.PubSubAPI.publish(1, {some : "data", other : "all the time" })}, 2000);
+    setTimeout(function() {RMGL.PubSubAPI.publish(0, {some : "to", other : "publishing channel" })}, 3000);
+    setTimeout(function() {RMGL.PubSubAPI.publish(1, {some : "just keep", other : "feeding" })}, 4000);
+    setTimeout(function() {RMGL.PubSubAPI.publish(0, {some : "like", other : "so" })}, 5000);
+    setTimeout(function() {RMGL.PubSubAPI.publish(1, {some : "with", other : "data" })}, 6000);
+    setTimeout(function() {RMGL.PubSubAPI.publish(0, {some : "and", other : "with" })}, 7000);
+    setTimeout(function() {RMGL.PubSubAPI.publish(1, {some : "more", other : "data" })}, 8000);
+  }
   
-  // A handle on the default grid in mstGrids class
+  // A handle on the default grid
   grid = RMGL.mstGrids.getGrid("defaultGrid");
   
   componentWillMount(){
-
-    // Setting up layout and initial subscription data to enable adding the grid
+    // Setting up layouts and initial subscription data
     // i is the component index, the rest of the params are react-grid-layout standard
-    let layoutMap = { i: '0', x: 0, y: 0, w: 4, h: 3};
-    this.grid.addMstGridItem( "simple", layoutMap, {data: "initial data" });
-
-
+    let layoutMap = { i: '0', x: 0, y: 0, w: 5, h: 5};
+    this.grid.addMstGridItem( "simple", layoutMap, {some : "initial data", other : "more data" });
+    layoutMap = { i: '1', x: 5, y: 0, w: 5, h: 5};
+    this.grid.addMstGridItem( "simple", layoutMap, {some : "and even", other : "more data" });
   }
 
-
-  
   render() {
     return (
       <div className="App">
         <header style={{height:'6em'}}>
-          <p>The simplest possible demo of react-mst-grid-layout</p>
+          <p>The simplest possible demo of <a href="https://github.com/svenberglund/react-mst-grid-layout"><code>react-mst-grid-layout</code></a></p>
         </header>
-
-
         <RMGL.MstGridLayout  // The grid component
           compactType="vertical" // default : none
           breakpoint="lg" // default : 'lg' = 12 columns
           rowHeight={30} // default : 30
           gridStyle={{ backgroundColor: 'LightSteelBlue' }}
         />
-
-
-
-
-
+        <br/>
+        <form>
+        <input type="button" value="Publish data!" onClick={this.handleSubmit}/>      
+        </form>
       </div>
     );
   }
 }
 
-// Wiring a publish channel (1) to our grid item (1)
+// Wiring publish channels to our grid items
+RMGL.PubSubAPI.subscribe("defaultGrid",0, 0 );
 RMGL.PubSubAPI.subscribe("defaultGrid",1, 1 );
 
 export default App;
--- minimal-demo/src/elements/GridElementDeadSimple.js
+++ minimal-demo/src/elements/GridElementDeadSimple.js
@@ -4,13 +4,13 @@ export default class GridElementAwsome{
         static renderElement(i, sm) {
 
             // The only rule here is that you shall use the i parameter as key for the enclosing div.
-            // with sm (the subscription data map) you do what ever you want, see below
+            // with sm (the subscription data map) you do what ever you want
 
                 let listItems =[...sm.keys()].map((key) =>
                         <li key={key}>{key} : {sm.get(key)}</li>
                 );
-                return <div key={i}>
-                    <p>This is a generic listening grid element - replace it with your iplementation.</p>
+                return <div key={i} style={{backgroundColor: 'Cornsilk' }}>
+                    <p>This is a dead simple generic listening grid element.</p>
                     <ul>{listItems}</ul>
                 </div>;
         };
