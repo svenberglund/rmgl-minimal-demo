83571a6e0d4cf46fc0091c6cb859dabdc02ece07
Sven Berglund <sven.3000.berglund@gmail.com>
2019-09-02 18:46:20 -0400

    Making the publishig to channels a little prettier with chained promises

--- minimal-demo/src/App.js
+++ minimal-demo/src/App.js
@@ -4,7 +4,7 @@ import RMGL from 'react-mst-grid-layout';
 import GridElementDeadSimple from './elements/GridElementDeadSimple'
 
 // Register our grid element implementation with a name
-RMGL.GridElementSuper.register("simple", GridElementDeadSimple);
+GridElementDeadSimple.register("simple", GridElementDeadSimple);
 
 class App extends Component {
   constructor(props) {
@@ -12,17 +12,33 @@ class App extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // Publishing mock data to the grid
+  data =[{some : "publish", other : "more" },
+  {some : "data", more : "all the time" },
+  {some : "to", other : "publishing channel"}, 
+  {some : "just keep", more : "feeding" }, 
+  {some : "like", other : "so" },
+  {some : "with", more : "data" },
+  {some : "and", other : "with" },
+  {some : "more", more : "data" },
+  ];
+
+  getPromise = () => new Promise((resolve, reject) => {
+    setTimeout(resolve,250)
+  })
+
+
+  // Publishing data data to the grid on channels 0 and 1
   handleSubmit(event){
-    RMGL.PubSubAPI.publish(1, {some : "awesome", other : "we can" });
-    setTimeout(function() {RMGL.PubSubAPI.publish(0, {some : "publish", other : "more" })}, 1000);
-    setTimeout(function() {RMGL.PubSubAPI.publish(1, {some : "data", other : "all the time" })}, 2000);
-    setTimeout(function() {RMGL.PubSubAPI.publish(0, {some : "to", other : "publishing channel" })}, 3000);
-    setTimeout(function() {RMGL.PubSubAPI.publish(1, {some : "just keep", other : "feeding" })}, 4000);
-    setTimeout(function() {RMGL.PubSubAPI.publish(0, {some : "like", other : "so" })}, 5000);
-    setTimeout(function() {RMGL.PubSubAPI.publish(1, {some : "with", other : "data" })}, 6000);
-    setTimeout(function() {RMGL.PubSubAPI.publish(0, {some : "and", other : "with" })}, 7000);
-    setTimeout(function() {RMGL.PubSubAPI.publish(1, {some : "more", other : "data" })}, 8000);
+    RMGL.PubSubAPI.publish(1, {some : "Awesome!", other : "Now we can" });
+    var myPromise = this.getPromise();
+    for(let i=0; i<40; i++){
+      myPromise = myPromise.then(
+        () => {
+          RMGL.PubSubAPI.publish(i%2, this.data[i%8]);
+          return this.getPromise() // a neat little trick for chaining. 
+        }
+      )
+    }
   }
   
   // A handle on the default grid
@@ -34,7 +50,7 @@ class App extends Component {
     let layoutMap = { i: '0', x: 0, y: 0, w: 5, h: 5};
     this.grid.addMstGridItem( "simple", layoutMap, {some : "initial data", other : "more data" });
     layoutMap = { i: '1', x: 5, y: 0, w: 5, h: 5};
-    this.grid.addMstGridItem( "simple", layoutMap, {some : "and even", other : "more data" });
+    this.grid.addMstGridItem( "simple", layoutMap, {some : "and even", more : "more data" });
   }
 
   render() {
